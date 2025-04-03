822bdf7d258fb37655d796214b838a166bc975c3
Pongchanan <pongchanansriwanna2547@gmail.com>
2025-04-04 02:15:11 +0700

    Refactor MQTT client setup to run in a separate thread

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from . import models
 from .routers import locker, organization, service_request, log, qr_gen
 from fastapi.middleware.cors import CORSMiddleware
 
+import threading
 import paho.mqtt.client as mqtt
 from .mqtt.client import on_connect, on_message
 
@@ -12,17 +13,21 @@ from .mqtt.client import on_connect, on_message
 broker = "152.42.220.156"  # The IP address of your MQTT broker
 port = 1883  # Default MQTT port (use 8883 if SSL/TLS is enabled)
 
-# Set up the client and assign the callbacks
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+def start_mqtt_client():
+    # Set up the client and assign the callbacks
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-# Connect to the broker
-client.connect(broker, port, 60)
+    # Connect to the broker
+    client.connect(broker, port, 60)
 
-# Loop forever to listen for messages
-client.loop_forever()
+    # Loop forever to listen for messages
+    client.loop_forever()
 
+# Start the MQTT client in a separate thread
+mqtt_thread = threading.Thread(target=start_mqtt_client, daemon=True)
+mqtt_thread.start()
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -46,7 +51,6 @@ app.include_router(log.router)
 app.include_router(organization.router)
 app.include_router(service_request.router)
 app.include_router(qr_gen.router)
-
 
 @app.get("/")
 def root():
